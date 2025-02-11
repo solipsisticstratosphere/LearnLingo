5dfd047b089ad0ea2d7734543714a742267594b8
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-02-12 00:54:54 +0200

    added .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1WAzPDPL2wA2tYSQIYFoL04JHiz8gNo8",
-  authDomain: "learnlingo-96439.firebaseapp.com",
-  projectId: "learnlingo-96439",
-  storageBucket: "learnlingo-96439.firebasestorage.app",
-  messagingSenderId: "84162327431",
-  appId: "1:84162327431:web:6a00f7793424706656d922",
-  measurementId: "G-N03N4JFLGG",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
